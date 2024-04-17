f187fb742c3f817debfc610e906981abfc14a2b6
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2024-04-17 11:55:57 +0900

    [Gold III] Title: 채굴, Time: 3464 ms, Memory: 225772 KB -BaekjoonHub

--- 백준/Gold/15573. 채굴/채굴.py
+++ 백준/Gold/15573. 채굴/채굴.py
@@ -1,67 +1,60 @@
 import sys
 from collections import deque
 
-# print = sys.stdout.write
-input = sys.stdin.readline
+n, m, k = map(int, sys.stdin.readline().split())
 
-# 반드시 아래 두 라인 주석처리 후 업로드
-# f = open("input.txt", "rt")
-# input = f.readline
-# 위 두라인 주석처리 후 업로드
-
-DIRECTIONS = ((-1, 0), (1, 0), (0, -1), (0, 1))
-
-N, M, K = map(int, input().split())
-maps = [list(map(int, input().split())) for _ in range(N)]
+stone = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
 
-def main():
-    start, end = 1, 10**6
+def bfs(d):
+    queue = deque()
+    visited = [[False] * m for _ in range(n)]
+    dx, dy = [-1, 0, 1, 0], [0, -1, 0, 1]
+    cnt = 0
 
-    while start < end:
-        d = (start + end) >> 1
-
-        if BFS(d) < K:
-            start = d + 1
-        else:
-            end = d
-
-    print(end)
-
-
-def prepare_mining(d, v):
-    q = deque()
-    for i in range(N):
-        for j in (0, M - 1):
-            if maps[i][j] <= d:
-                q.append((i, j))
-                v[i][j] = True
-
-    for i in range(1, M - 1):
-        if maps[0][i] <= d:
-            q.append((0, i))
-            v[0][i] = True
-    return q
-
-
-def BFS(d):
-    v = [[False] * M for _ in range(N)]
-    q = prepare_mining(d, v)
-    cnt = len(q)
-
-    while q:
-        y, x = q.popleft()
-
-        for dy, dx in DIRECTIONS:
-            ny, nx = dy + y, dx + x
-            if ny >= N or nx >= M or ny < 0 or nx < 0 or v[ny][nx] or maps[ny][nx] > d:
-                continue
-            q.append((ny, nx))
-            v[ny][nx] = True
+    for i in range(m):
+        visited[0][i] = True
+        if stone[0][i] <= d:
+            queue.append((0, i))
             cnt += 1
+    for i in range(1, n):
+        visited[i][0] = True
+        if stone[i][0] <= d:
+            queue.append((i, 0))
+            cnt += 1
+        visited[i][m-1] = True
+        if stone[i][m-1] <= d:
+            queue.append((i, m-1))
+            cnt += 1
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if visited[nx][ny]:
+                continue
+
+            visited[nx][ny] = True
+            if stone[nx][ny] <= d:
+                queue.append((nx, ny))
+                cnt += 1
 
     return cnt
 
 
-if __name__ == "__main__":
-    main()
+start, end = 1, 10**6
+min_d = end
+while start <= end:
+    mid = (start + end) // 2
+    my_k = bfs(mid)
+
+    if my_k >= k:
+        min_d = min(min_d, mid)
+        end = mid - 1
+    else:
+        start = mid + 1
+
+sys.stdout.write(str(min_d))
