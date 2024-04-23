b4062d02ac2f0661205ee0a8e2329ccad74c23c8
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2024-04-23 11:37:47 +0900

    [Gold IV] Title: 미로만들기, Time: 48 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,45 +1,40 @@
 import sys
-from collections import deque
+import heapq
 
-# print = sys.stdout.write
 input = sys.stdin.readline
 INF = float("inf")
-# 반드시 아래 두 라인 주석 처리 후 제출
-# f = open("input.txt", "rt")
-# input = f.readline
-# 반드시 위 두 라인 주석 처리 후 제출
 
 N = int(input().rstrip())
 board = [list(input().rstrip()) for _ in range(N)]
-v = [[INF] * N for _ in range(N)]
+dist = [[INF] * N for _ in range(N)]
 
 
-def BFS():
-    q = deque()
-    q.append((0, 0, 0))  # 좌표 + 벽 부순 횟수
-    v[0][0] = 0
+def dijkstra():
+    q = []
+    heapq.heappush(q, (0, 0, 0))  # 벽 부순 횟수 좌표
+    dist[0][0] = 0
     while q:
-        y, x, cnt = q.popleft()
-        if (y, x) == (N - 1, N - 1):
+        cnt, y, x = heapq.heappop(q)
+        if dist[y][x] < cnt:
             continue
 
         for dy, dx in (-1, 0), (1, 0), (0, -1), (0, 1):
             ny, nx = dy + y, dx + x
-            if ny >= N or nx >= N or ny < 0 or nx < 0 or v[ny][nx] <= cnt:
+            if ny >= N or nx >= N or ny < 0 or nx < 0 or dist[ny][nx] <= cnt:
                 continue
             if board[ny][nx] == "1":
-                q.append((ny, nx, cnt))
-                v[ny][nx] = cnt
+                heapq.heappush(q, (cnt, ny, nx))
+                dist[ny][nx] = cnt
                 continue
 
-            if board[ny][nx] == "0" and v[ny][nx] > cnt + 1:
-                q.append((ny, nx, cnt + 1))
-                v[ny][nx] = cnt + 1
+            if board[ny][nx] == "0" and dist[ny][nx] > cnt + 1:
+                heapq.heappush(q, (cnt + 1, ny, nx))
+                dist[ny][nx] = cnt + 1
 
 
 def main():
-    BFS()
-    print(v[N - 1][N - 1])
+    dijkstra()
+    print(dist[N - 1][N - 1])
 
 
 if __name__ == "__main__":
