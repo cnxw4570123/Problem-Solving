3f26986365454f34611bdc409dd51a909afc5b5b
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2024-05-31 11:36:46 +0900

    [Gold IV] Title: 팰린드롬?, Time: 936 ms, Memory: 171860 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -0,0 +1,41 @@
+import sys
+
+# print = sys.stdout.write
+input = sys.stdin.readline
+# 반드시 아래 두 라인 주석 처리 후 제출
+# f = open("input.txt", "rt")
+# input = f.readline
+# 반드시 위 두 라인 주석 처리 후 제출
+N = int(input().rstrip())
+arr = [0] + list(map(int, input().split()))
+M = int(input().rstrip())
+dp = [[1 if i == j else -1 for j in range(N + 1)] for i in range(N + 1)]
+
+
+def find(s, e):
+    if s > e:
+        return 1  # 결과에 영향을 미치지 않음
+
+    if dp[s][e] != -1:
+        return dp[s][e]
+
+    if s + 1 == e:
+        dp[s][e] = is_same(s, e)
+        return dp[s][e]
+
+    dp[s][e] = find(s + 1, e - 1) & is_same(s, e)
+    return dp[s][e]
+
+
+def is_same(s, e):
+    return 1 if arr[s] == arr[e] else 0
+
+
+def main():
+    for i in range(M):
+        s, e = map(int, input().split())
+        print(find(s, e))
+
+
+if __name__ == "__main__":
+    main()
