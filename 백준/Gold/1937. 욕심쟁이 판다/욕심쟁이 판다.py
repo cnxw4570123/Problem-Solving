4f7c7613a1e3bb93dede1a1ef537953ded8eab1e
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2024-06-20 13:04:04 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 332 ms, Memory: 206416 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 # 반드시 아래 두 라인 주석 처리 후 제출
 # f = open("input.txt", "rt")
 # input = f.readline
-# 반드시 위 두 라인 주석 처리 후 제출5
+# 반드시 위 두 라인 주석 처리 후 제출
 DIRECTIONS = (-1, 0), (1, 0), (0, -1), (0, 1)
 
 N = int(input().rstrip())
@@ -13,25 +13,19 @@ _map = [list(map(int, input().split())) for _ in range(N)]
 dp = [[0] * N for _ in range(N)]
 
 
-def DFS(y, x, v):
+def DFS(y, x):
     global dp
-    if not dp[y][x]:
-        dp[y][x] = 1
-    v.add((y, x))
+    if dp[y][x]:
+        return dp[y][x]
+
+    dp[y][x] = 1
     food = _map[y][x]
     for dy, dx in DIRECTIONS:
         ny, nx = dy + y, dx + x
-        if (
-            ny >= N
-            or nx >= N
-            or ny < 0
-            or nx < 0
-            or _map[ny][nx] <= food
-            # or (ny, nx) in v
-        ):
+        if ny >= N or nx >= N or ny < 0 or nx < 0 or _map[ny][nx] <= food:
             continue
 
-        dp[y][x] = max(dp[y][x], (dp[ny][nx] if dp[ny][nx] else DFS(ny, nx, v)) + 1)
+        dp[y][x] = max(dp[y][x], DFS(ny, nx) + 1)
 
     return dp[y][x]
 
@@ -41,7 +35,7 @@ def main():
         for j in range(N):
             if dp[i][j]:
                 continue
-            DFS(i, j, set())
+            DFS(i, j)
 
     print(max([max(dp[i]) for i in range(N)]))
 
