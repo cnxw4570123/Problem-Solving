41cd39212eb4968d0db2852c15921536952a7823
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2024-10-15 14:40:43 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 452 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -5,35 +5,37 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-sys.setrecursionlimit(10000)
 
-graph = [[] for _ in range(N + 1)]
-visited = [False] * (N + 1)
-for _ in range(M):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+parent = [i for i in range(N + 1)]
 
 
-def DFS(u):
-    global visited
-    if visited[u]:
+def find(x):
+    if parent[x] == x:
+        return x
+
+    parent[x] = find(parent[x])
+    return parent[x]
+
+
+def union(x, y):
+    x, y = find(x), find(y)
+
+    if x == y:
         return
 
-    visited[u] = True
-    for next in graph[u]:
-        DFS(next)
+    if x > y:
+        parent[x] = y
+        return
+
+    parent[y] = x
 
 
 def main():
-    ans = 0
-    for i in range(1, N + 1):
-        if visited[i]:
-            continue
-        DFS(i)
-        ans += 1
-
-    print(ans)
+    for _ in range(M):
+        u, v = map(int, input().split())
+        union(u, v)
+    ans = [find(i) for i in range(1, N + 1)]
+    print(len(set(ans)))
 
 
 if __name__ == "__main__":
