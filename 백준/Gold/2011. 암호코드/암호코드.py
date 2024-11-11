733db049323948dbf3ce13f80af8370a53e6cbc7
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2024-11-11 14:48:10 +0900

    [Gold V] Title: 암호코드, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.py
+++ 백준/Gold/2011. 암호코드/암호코드.py
@@ -4,38 +4,29 @@ import sys
 # print = sys.stdout.write
 input = sys.stdin.readline
 
-
 MOD = 1_000_000
-code = "0" + input().rstrip()
+code = [0] + list(map(int, input().rstrip()))
 dp = [0] * (len(code) + 1)
 # 점화식 처리하기 위한 장치
 dp[0] = 1
 
 
 def main():
-    # 순서상 문자열의 두 번째
-    # dp의 첫 번째
-    # dp[1] = 1
+    if code[1] == 0:
+        print(0)
+        return
 
     for idx in range(1, len(code)):
-        prev, current = int(code[idx - 1]), int(code[idx])
-        if current == 0:
-            # 두 부분을 합쳐도 문자 성립이 안되면
-            if prev * 10 + current > 26 or prev * 10 + current == 0:
-                print(0)
-                return
-            # 두 부분을 합치면 문자 생성은 가능
-            dp[idx] = dp[idx - 2] % MOD
-            continue
+        # 길이를 늘려줬으므로 첫 번째 자리부터 시작가능
+        if code[idx] > 0:
+            dp[idx] += dp[idx - 1]
 
-        # 현재 숫자만 문자로 변환 가능
-        if prev * 10 + current > 26 or prev * 10 + current < 10:
-            dp[idx] = dp[idx - 1] % MOD
-            continue
+        temp = code[idx - 1] * 10 + code[idx]
+        if 10 <= temp <= 26:
+            dp[idx] += dp[idx - 2]
+        dp[idx] %= MOD
 
-        # 모두 가능
-        dp[idx] = (dp[idx - 2] + dp[idx - 1]) % MOD
-
+    # 강제로 한 글자 늘렸으니 -1 해줘야함
     print(dp[len(code) - 1] % MOD)
 
 
