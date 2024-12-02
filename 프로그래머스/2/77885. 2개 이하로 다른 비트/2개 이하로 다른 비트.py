900d219ddd02a44bdef114e2019108b69d9d7b6c
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2024-12-02 15:50:49 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 491.47 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,14 +1,30 @@
+def find(number):
+    binary = bin(number)[2:]
+    # 모든 하위 비트가 꽉 찼을 때 -> 최상위 비트 추가
+    if binary.count("1") == len(binary):
+        temp = number | (1 << len(binary))
+
+        candidate = temp & ~(1 << len(binary) - 1)
+        return candidate
+
+    # 하위 비트에 꺼진 것이 있을 경우
+
+    # 그 중 최소 비트가 꺼져있으면
+    if number & (1 << 0) == 0:
+        candidate = number | (1 << 0)
+        return candidate
+
+    for idx in range(len(binary) - 1, 1, -1):
+        if binary[idx] != "1" or binary[idx - 1] != "0":
+            continue
+        # 켜진 하위 비트 끄기
+        candidate = number & ~(1 << (len(binary) - idx - 1))
+        # 꺼진 상위 비트 켜기
+        candidate |= 1 << (len(binary) - idx)
+        return candidate
+    
 def solution(numbers):
     ans = []
     for number in numbers:
-        binary = bin(number)[2:]
-        if binary.count("1") == len(binary):
-            temp = number | (1 << len(binary))
-            ans.append(temp & ~(1 << len(binary) - 1))
-            continue
-        
-        for idx in range(len(binary) + 1):
-            if 1 << idx & number == 0:
-                ans.append(number | (1 << idx))
-                break
+        ans.append(find(number))
     return ans
--- 프로그래머스/2/77885. 2개 이하로 다른 비트/README.md
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/README.md
@@ -1,6 +1,6 @@
 # [level 2] 2개 이하로 다른 비트 - 77885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77885#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77885#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 02일 15:48:46
+2024년 12월 02일 15:50:47
 
 ### 문제 설명
 
