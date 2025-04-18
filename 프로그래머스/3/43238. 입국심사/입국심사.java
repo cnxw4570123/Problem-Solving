915d340372b7f7ecb6f676b002152cc68fc3ad38
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2025-04-18 15:59:09 +0900

    [level 3] Title: 입국심사, Time: 145.33 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,26 +2,28 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
+        long answer = 0;
         Arrays.sort(times);
-        long answer = lowerBound(times, n);
-        return answer;
+        return find(n, times);
     }
     
-    static long lowerBound(int[] times, int n){
-        long start = 1, end = (long) times[times.length - 1] * n;
+    static long find(int people, int[] times){
+        long start = 1, end = (long)people * times[times.length - 1] + 1;
+        
         while(start < end){
-            long mid = (start + end) / 2;
-			long count = 0;
-            for(int time : times){
-                count += mid / time;
+			long mid = (start + end) >> 1;
+            long possibleCnt = 0;
+            for(int i = 0; i < times.length; i++){
+                possibleCnt += mid / times[i];
             }
-			
-            if(count < n){
-				start = mid + 1;
-            } else{
+            
+            if(possibleCnt >= people){
                 end = mid;
+                continue;
             }
+            start = mid + 1;
         }
+        
         return end;
     }
 }
