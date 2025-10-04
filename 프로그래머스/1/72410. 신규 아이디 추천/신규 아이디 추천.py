d341cf5c70d7c8ebc07b2077935659e1afad42b8
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2025-10-04 21:18:07 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.47 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -3,19 +3,16 @@ def solution(new_id):
     ans = new_id.lower()
     ans = re.sub(r"[^a-z0-9-_.]", "", ans)
     ans = re.sub(r"\.{2,}", ".", ans)
-
-    if ans.startswith("."):
-        ans = ans[1:]
-    if ans.endswith("."):
-        ans = ans[:-1]
+	
+    ans = ans.strip(".")
+    
     
     if not ans:
         ans = "a"
+        
     if len(ans) >= 16:
-        ans = ans[:15]
-        if ans[-1] == ".":
-            ans = ans[:-1]
-	
+        ans = ans[:15].rstrip(".")
+        
     if len(ans) <= 2:
         ans += ans[-1] * (3 - len(ans))
     
