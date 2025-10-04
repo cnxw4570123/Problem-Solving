e2fb5002bd2d1d64d33d489ab049dbf63bc3b100
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2025-10-04 20:24:44 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 599.40 ms, Memory: 63.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,16 @@
+def solution(elements):
+    MAX = len(elements)
+    answer = set()
+    dp = [[0] * (MAX + 1) for _ in range(MAX + 1)]
+
+    for i in range(1, MAX + 1):
+        for w in range(1, MAX + 1):
+            if i + w - 1 > MAX:
+                dp[i][w] = dp[i][MAX - i + 1] + dp[1][w + i - MAX - 1]
+            else:
+                dp[i][w] = dp[i][w - 1] + elements[i + w - 2]
+            answer.add(dp[i][w])
+            
+    
+    
+    return len(answer)
