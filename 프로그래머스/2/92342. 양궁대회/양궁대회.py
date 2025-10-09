b04fa887d073fcd454d1617bd694dd5c9bd5aaa7
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2025-10-09 15:13:55 +0900

    [level 2] Title: 양궁대회, Time: 846.75 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -1,47 +1,20 @@
-from collections import defaultdict
-max_diff, answer = 0, [0] * 11
+from itertools import combinations_with_replacement
+from collections import Counter
 def solution(n, info):
-    apeach = sum([10 - i for i in range(11) if info[i]])
-    DFS(info, n, 0, apeach, 0)
-    if not max_diff:
-        return [-1]
-    return answer
-
-def DFS(info, arrow_left, idx, other, points, history = [0] * 11):
-    global answer, max_diff
-    # 점수 계산
-    if not arrow_left:
-        if max_diff < (points - other):
-            max_diff = points - other
-            answer = [history[i] for i in range(11)]
-            return
-        
-        if max_diff == (points - other):
-            print(
-            f"""기록 : {answer}\n대체재 : {history}""")
-            for i in range(10, -1, -1):
-                if answer[i] == history[i]:
-                    continue
-                if answer[i] > history[i]:
-                    return
-                answer = [history[i] for i in range(11)]
-        return
-    
-    for i in range(idx, 11):
-        # 남은 화살 + 1보다 어피치가 맞춘 화살 개수가 더 크면 점수 포기
-        if info[i] + 1 > arrow_left:
-            continue
-        history[i] = info[i] + 1
-        new_history = [history[i] for i in range(11)]
-        if info[i]:
-            DFS(info, arrow_left - history[i], i + 1, other - 10 + i, points + 10 - i, new_history)
-        else:
-            DFS(info, arrow_left - history[i], i + 1, other, points + 10 - i, new_history)
-        history[i] = 0
-    else:
-        history[10] += arrow_left
-        new_history = [history[i] for i in range(11)]
-        DFS(info, 0, 11, other, points, new_history)
-        history[10] = 0
-        return
-        
+    maxdiff,max_comb=0,{}
+    for combi in combinations_with_replacement(range(11), n):
+        cnt=Counter(combi)
+        score1, score2=0,0
+        for i in range(1, 11):
+            if info[10-i]<cnt[i]: score1+=i
+            elif info[10-i]>0: score2+=i
+        diff=score1-score2
+        if diff>maxdiff:
+            max_comb=cnt
+            maxdiff=diff
+    if maxdiff>0:
+        answer=[0]*11
+        for n in max_comb:
+            answer[10-n]=max_comb[n]
+        return answer
+    else: return [-1]
