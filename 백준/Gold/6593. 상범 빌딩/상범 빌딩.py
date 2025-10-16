4126c592a3a0c7e04551e36421c0507c003fb3b0
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2025-10-16 22:18:53 +0900

    [Gold V] Title: 상범 빌딩, Time: 96 ms, Memory: 35016 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
@@ -1,54 +1,62 @@
 import sys
 from collections import deque
 
+# print = sys.stdout.write
 input = sys.stdin.readline
+
+DIRECTIONS = (-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)
+
+
+def main():
+    ans = []
+    while init():
+        ans.append(BFS())
+    print("\n".join(ans))
+
+
+def init():
+    global L, R, C, board, q, v
+    L, R, C = map(int, input().split())
+    if (L, R, C) == (0, 0, 0):
+        return False
+
+    board, q = [], deque()
+    v = [[[False] * C for _ in range(R)] for _ in range(L)]
+
+    for i in range(L):
+        floor = []
+        for j in range(R):
+            row = list(input().rstrip())
+            for k in range(C):
+                if row[k] == "S":
+                    q.append((i, j, k, 0))
+                    v[i][j][k] = True
+            floor.append(row)
+        board.append(floor)
+        input()
+
+    return True
 
 
 def BFS():
-    global ans
-    v = [[[False for _ in range(C)] for _ in range(R)] for _ in range(L)]
+
     while q:
-        cur = q.popleft()
-        if buildings[cur[0]][cur[1]][cur[2]] == "E":
-            ans.append(f"Escaped in {cur[3]} minute(s).")
-            return
-        if v[cur[0]][cur[1]][cur[2]]:
-            continue
-        v[cur[0]][cur[1]][cur[2]] = True
+        z, y, x, t = q.popleft()
 
-        for i in range(6):
-            nz, ny, nx = cur[0] + dz[i], cur[1] + dy[i], cur[2] + dx[i]
-            if (
-                nz >= L
-                or ny >= R
-                or nx >= C
-                or nz < 0
-                or ny < 0
-                or nx < 0
-                or v[nz][ny][nx]
-                or buildings[nz][ny][nx] == "#"
-            ):
+        if board[z][y][x] == "E":
+            return f"Escaped in {t} minute(s)."
+
+        for dz, dy, dx in DIRECTIONS:
+            nz, ny, nx = dz + z, dy + y, dx + x
+            if nz >= L or ny >= R or nx >= C or nz < 0 or ny < 0 or nx < 0:
                 continue
-            q.append([nz, ny, nx, cur[3] + 1])
-    ans.append("Trapped!")
+            if v[nz][ny][nx] or board[nz][ny][nx] == "#":
+                continue
+            q.append((nz, ny, nx, t + 1))
+            v[nz][ny][nx] = True
+
+    return "Trapped!"
 
 
-dz, dy, dx = [-1, 0, 0, 0, 0, 1], [0, -1, 1, 0, 0, 0], [0, 0, 0, -1, 1, 0]
-ans = []
-while True:
-    q = deque()
-    L, R, C = map(int, input().rstrip().split())
-    if (L, R, C) == (0, 0, 0):
-        break
-    buildings = []
-    for i in range(L):
-        temp = []
-        for j in range(R):
-            col = input().rstrip()
-            if col.find("S") != -1:
-                q.append([i, j, col.index("S"), 0])
-            temp.append(col)
-        buildings.append(temp)
-        input()
-    BFS()
-print("\n".join(ans))
+if __name__ == "__main__":
+    main()
