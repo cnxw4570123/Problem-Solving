c09d1cff87a5f43980b46f9fd5524828f6911ab1
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2025-11-17 17:03:40 +0900

    [Gold III] Title: 파티, Time: 52 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -10,50 +10,46 @@ INF = sys.maxsize
 def main():
     init()
 
-    for i in range(1, N + 1):
-        dijkstra(i)
+    dist, rev_dist = dijkstra(X, graph), dijkstra(X, reverse_graph)
 
-    ans = 0
-    for i in range(1, N + 1):
-        ans = max(ans, dist[i][X] + dist[X][i])
-
+    ans = max([dist[i] + rev_dist[i] for i in range(1, N + 1)])
     print(ans)
 
 
-def dijkstra(node):
-    global dist
+def dijkstra(node, g):
+    res = [INF] * (N + 1)
 
     hq = [(0, node)]
 
-    dist[node][node] = 0
+    res[node] = 0
 
     while hq:
         cost, current = heappop(hq)
 
-        if dist[node][current] < cost:
+        if res[current] < cost:
             continue
 
-        for next_node, next_cost in graph[current]:
+        for next_node, next_cost in g[current]:
             new_cost = next_cost + cost
 
-            if dist[node][next_node] <= new_cost:
+            if res[next_node] <= new_cost:
                 continue
 
-            dist[node][next_node] = new_cost
+            res[next_node] = new_cost
             heappush(hq, (new_cost, next_node))
+    return res
 
 
 def init():
-    global N, M, X, graph, dist
+    global N, M, X, graph, reverse_graph
     N, M, X = map(int, input().split())
     graph = [[] for _ in range(N + 1)]
-
+    reverse_graph = [[] for _ in range(N + 1)]
     for _ in range(M):
         _from, to, cost = map(int, input().split())
 
         graph[_from].append((to, cost))
-
-    dist = [[INF] * (N + 1) for _ in range(N + 1)]
+        reverse_graph[to].append((_from, cost))
 
 
 if __name__ == "__main__":
