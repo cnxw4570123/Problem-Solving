be408db4ccebe0e0189937ef663581fea1bec5ea
LateNightWorker <96401835+cnxw4570123@users.noreply.github.com>
2025-11-19 14:38:04 +0900

    [Gold II] Title: 면접보는 승범이네, Time: 1092 ms, Memory: 177932 KB -BaekjoonHub

---
+++ 백준/Gold/17835. 면접보는 승범이네/면접보는 승범이네.py
@@ -0,0 +1,64 @@
+import sys
+from collections import defaultdict
+from heapq import heappush, heappop
+
+# print = sys.stdout.write
+input = sys.stdin.readline
+
+INF = float("inf")
+
+
+def main():
+    init()
+    dist = dijkstra()
+    ans = [0, 0]
+    for i in range(1, N + 1):
+        if dist[i] <= ans[1]:
+            continue
+        ans[0], ans[1] = i, dist[i]
+
+    print(ans[0])
+    print(ans[1])
+
+
+def dijkstra():
+    hq = []
+    dist = [INF] * (N + 1)
+    for place in interview_places:
+        dist[place] = 0
+        heappush(hq, (0, place))
+
+    while hq:
+        cost, current = heappop(hq)
+
+        if dist[current] < cost:
+            continue
+
+        for next_node, next_cost in graph[current].items():
+            new_cost = next_cost + cost
+
+            if dist[next_node] <= new_cost:
+                continue
+            dist[next_node] = new_cost
+            heappush(hq, (new_cost, next_node))
+
+    return dist
+
+
+def init():
+    global N, M, K, graph, interview_places
+    N, M, K = map(int, input().split())
+
+    graph = defaultdict(dict)
+
+    for _ in range(M):
+        u, v, c = map(int, input().split())
+        if graph[v].get(u, INF) <= c:
+            continue
+        graph[v][u] = c
+
+    interview_places = list(map(int, input().split()))
+
+
+if __name__ == "__main__":
+    main()
